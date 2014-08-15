9d5561462c74bfdb12d3656ce7a5a2f3736b562c
vagrant <vagrant@cs162-fall2014>
2014-08-15 08:40:59 +0000

    tabs => spaces

--- hw0/map.c
+++ hw0/map.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int foo;			/* a staticly allocated variable */
+int foo;            /* a staticly allocated variable */
 
-int recur(int i) {		/* a recursive function */
-  int j = i;			/* a stack allocated variable within a recursive function */
+int recur(int i) {      /* a recursive function */
+  int j = i;            /* a stack allocated variable within a recursive function */
   printf("recur call %d: stack@ %lx\n", i, j); /* fix this so it prints the address of j */
   if (i > 0) {
     return recur(i-1);
@@ -12,12 +12,12 @@ int recur(int i) {		/* a recursive function */
   return 0;
 }
 
-int stuff = 7;			/* a statically allocarted, pre-initialized variable */
+int stuff = 7;          /* a statically allocarted, pre-initialized variable */
 
 int main (int argc, char *argv[]) {
-  int i;			/* a stack allocated variable */
-  char *buf1 = malloc(100);	/* dynamically allocate some stuff */
-  char *buf2 = malloc(100);	/* and some more stuff */
+  int i;            /* a stack allocated variable */
+  char *buf1 = malloc(100); /* dynamically allocate some stuff */
+  char *buf2 = malloc(100); /* and some more stuff */
   printf("_main  @ %lx\n", main); /* fix to print address of main */
   printf("recur @ %lx\n", recur); /* fix to print address of recur */
   printf("_main stack: %lx\n", i); /* fix to get address of the stack variable */
