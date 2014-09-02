459bcf36d342c511783f3255736102dd2dee1c77
Michael Ho <callmemikeh@gmail.com>
2014-09-01 23:57:48 -0700

    wc.c still erroring. fixed argv

--- personal/hw0/wc.c
+++ personal/hw0/wc.c
@@ -2,12 +2,13 @@
 #include <ctype.h>
 
 void wc(FILE *ofile, FILE *infile, char *inname) {
-    char cur_char;
+    char cur_char = fgetc(infile);
     int lines = 0;
     int words = 0;
     int chars = 0;
     int was_word = 0;
-    while(cur_char = fgetc(infile) != EOF) {
+    
+    while(cur_char != EOF) {
         chars++;
         if (was_word && isspace(cur_char)) {
             words++;
@@ -19,25 +20,29 @@ void wc(FILE *ofile, FILE *infile, char *inname) {
         if (cur_char == "\n") {
             lines++;
         }
+        cur_char = fgetc(infile);
     }
     char *final_string;
     sprintf(final_string, "%d %d %d %s", lines, words, chars, inname);
+    printf("%s", final_string);
     fprintf(ofile, final_string);
 }
 
 int main (int argc, char *argv[]) {
     FILE *ofile = stdout;
     FILE *infile;
+    printf("argc: %d", argc);
     switch (argc) {
     case 1:
-        infile = fopen(argv[0], "r");
-        wc(ofile, infile, argv[0]);
+        printf("Case 1: %d, %s", argc, argv[0]);
+        infile = fopen(argv[1], "r");
+        wc(ofile, infile, argv[1]);
         fclose(infile);
         break;
     case 2:
-        infile = fopen(argv[0], "r");
-        ofile = fopen(argv[1], "w");
-        wc(ofile, infile, argv[0]);
+        infile = fopen(argv[1], "r");
+        ofile = fopen(argv[2], "w");
+        wc(ofile, infile, argv[1]);
         fclose(infile);
         break;
     default:
