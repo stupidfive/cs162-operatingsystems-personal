512fe3b3b964466e6bbbfa4cec8b3b3a82222abc
Michael Ho <callmemikeh@gmail.com>
2014-09-02 10:52:29 -0700

    main.c works

--- personal/hw0/main.c
+++ personal/hw0/main.c
@@ -5,7 +5,10 @@
 
 int main() {
   struct rlimit lim;
-  printf("stack size: %d\n", getrlimit(RLIMIT_STACK, &lim));
-  printf("process limit: %d\n", getrlimit(RLIMIT_NPROC, &lim));
-  printf("max file descriptors: %d\n", getrlimit(RLIMIT_NOFILE, &lim));
+  getrlimit(RLIMIT_STACK, &lim);
+  printf("stack size: %d\n", lim.rlim_cur / 1024);
+  getrlimit(RLIMIT_NPROC, &lim);
+  printf("process limit: %d\n", lim.rlim_cur);
+  getrlimit(RLIMIT_NOFILE, &lim);
+  printf("max file descriptors: %d\n", lim.rlim_cur);
 }
