184bd01bdadd57375a10caa665889bb74b030839
Jason Jia <jasonjia@berkeley.edu>
2014-09-02 07:58:54 +0000

    Removing files

--- hw0/main.c
+++ hw0/main.c
@@ -5,10 +5,7 @@
 
 int main() {
     struct rlimit lim;
-    getrlimit(RLIMIT_STACK, &lim);
-    printf("stack size: %d\n",  (int) lim.rlim_cur);;
-    getrlimit(RLIMIT_NPROC, &lim);
-    printf("process limit: %d\n", (int) lim.rlim_cur);;
-    getrlimit(RLIMIT_NOFILE, &lim);
-    printf("max file descriptors: %d\n", (int) lim.rlim_cur);;
+    printf("stack size: %d\n",  0);
+    printf("process limit: %d\n", 0);
+    printf("max file descriptors: %d\n", 0);
 }
--- hw0/wc.c
+++ hw0/wc.c
@@ -2,41 +2,9 @@
 #include <ctype.h>
 
 void wc(FILE *ofile, FILE *infile, char *inname) {
-    int bytes = 0;
-    int words = 0;
-    int lines = 0;
-    int midWord = 0;
-    int buf = fgetc(infile);
-    while (buf != EOF) {
-        bytes++;
-        if (buf == '\n') {
-            lines++;
-        }
-        if (midWord && isspace(buf)) {
-            words++;
-            midWord = 0;
-        }
-        if (!isspace(buf)) {
-            midWord = 1;
-        }
-        buf = fgetc(infile);
-    }
-    fprintf(ofile, "%8d%8d%8d %s\n", lines, words, bytes, inname);
 }
 
 int main (int argc, char *argv[]) {
-    FILE *infile = stdin;
-    FILE *ofile = stdout;
-    char *name = "";
-    if (argc >= 2) {
-        name = argv[1];
-        infile = fopen(argv[1], "r");
-    }
-    if (argc >= 3) {
-        fopen(argv[2], "w");
-    }
-    wc(ofile, infile, name);
-    fclose(infile);
-    fclose(ofile);
+    
     return 0;
 }
