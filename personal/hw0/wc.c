7f7cd0ce9876ff186b1f563d893756d69a84788c
Michael Ho <callmemikeh@gmail.com>
2014-09-05 17:45:56 -0700

    fixed more things

--- personal/hw0/wc.c
+++ personal/hw0/wc.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
-void wc(FILE *ofile, FILE *infile, char *inname) {
+void wc(FILE *ofile, FILE *infile, char *inname, int size) {
     char cur_char = fgetc(infile);
     int lines = 0;
     int words = 0;
     int chars = 0;
     int was_word = 0;
-    
     while(cur_char != EOF) {
         chars++;
         if (was_word && isspace(cur_char)) {
@@ -17,36 +18,52 @@ void wc(FILE *ofile, FILE *infile, char *inname) {
         if (!isspace(cur_char)) {
             was_word = 1;
         }
-        if (cur_char == "\n") {
+        if (cur_char == '\n') {
             lines++;
         }
         cur_char = fgetc(infile);
     }
-    char *final_string;
-    sprintf(final_string, "%d %d %d %s", lines, words, chars, inname);
-    printf("%s", final_string);
-    fprintf(ofile, final_string);
+    int num_size = count_digits(lines) + count_digits(chars) + count_digits(words) + 3;
+    char *num_string = malloc(num_size);
+    sprintf(num_string, "%d %d %d ", lines, words, chars);
+    fputs(num_string, ofile);
+    fputs(inname, ofile);
+    fputs("\n", ofile);
+}
+int count_digits(int num) {
+    int count = 0;
+    while (num > 0) {
+        num /= 10;
+        count++;
+    }
+    return count;
 }
 
 int main (int argc, char *argv[]) {
     FILE *ofile = stdout;
-    FILE *infile;
-    printf("argc: %d", argc);
+    FILE *infile = stdin;
+    int size = 0;
     switch (argc) {
     case 2:
-        printf("Case 1: %d, %s", argc, argv[0]);
+        size = strlen(argv[1]);
         infile = fopen(argv[1], "r");
-        wc(ofile, infile, argv[1]);
+        wc(ofile, infile, argv[1], size);
         fclose(infile);
         break;
     case 3:
+        size = strlen(argv[1]);
         infile = fopen(argv[1], "r");
         ofile = fopen(argv[2], "w");
-        wc(ofile, infile, argv[1]);
+        wc(ofile, infile, argv[1], size);
         fclose(infile);
+        fclose(ofile);
+        break;
+    case 1:
+        wc(ofile, infile, "", size);
         break;
     default:
         printf("usage: ");
+        break;
     }
     return 0;
 }
