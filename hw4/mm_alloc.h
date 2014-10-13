3a0764a8ced78a604a5d1d867a5032c8741b986e
vaishaal shankar <vaishaal@gmail.com>
2014-10-13 09:24:46 -0700

    added some more skeleton

--- hw4/mm_alloc.h
+++ hw4/mm_alloc.h
@@ -6,6 +6,9 @@
 
 #pragma once
 
+ /* Define the block size since the sizeof will be wrong */
+#define BLOCK_SIZE 20
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -15,6 +18,35 @@ extern "C" {
 void* mm_malloc(size_t size);
 void* mm_realloc(void* ptr, size_t size);
 void mm_free(void* ptr);
+
+
+typedef struct s_block *s_block_ptr;
+
+/* block struct */
+struct s_block {
+    size_t size;
+    struct s_block *next;
+    struct s_block *prev;
+    int free;
+    void *ptr;
+    /* A pointer to the allocated block */
+    char data [1];
+ };
+
+/* Split block according to size, b must exist */
+void split_block (s_block_ptr b, size_t s);
+
+/* Try fusing block with neighbors */
+s_block_ptr fusion(s_block_ptr b);
+
+/* Get the block from addr */
+s_block_ptr get_block (void *p);
+
+/* Add a new block at the of heap,
+ * return NULL if things go wrong
+ */
+s_block_ptr extend_heap (s_block_ptr last , size_t s);
+
 
 #ifdef __cplusplus
 }
